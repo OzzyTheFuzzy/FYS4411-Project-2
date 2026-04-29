plotted the wavefunction and got the correct shape up to a normalization constant. The variance to the energy is low

src/PINN/PINN_vs_analytical.py
@@ -5,6 +5,8 @@ import numpy as np
 from training import Training
 from model import SE_Model, reconstruct_SE_model
 from pathlib import Path
+from scipy.integrate import trapezoid
+
 
 project_root = Path(__file__).resolve().parent
 
@@ -49,7 +51,7 @@ def evaluate_energy(model_name, positions):
     with torch.no_grad():
         u = model(positions) #calculate the log(pos) for the wavefunction for the given positions
 
-    return energy.detach().cpu().numpy()
+    return energy.detach().cpu().numpy(), u.detach().cpu().numpy() #return energy and log(pos) as numpy arrays for plotting
 
 
 def load_positions_and_energy(filename, device="cpu", index=None):
@@ -67,11 +69,14 @@ def load_positions_and_energy(filename, device="cpu", index=None):
         positions = positions[index:index+1]
         energies = energies[index:index+1]
 
-    return positions, energies.detach().cpu().numpy() # retrieve positions with torch and and return energies as numpy for plotting
+    return positions, energies.detach().cpu().numpy(), n_samples, N, dim # retrieve positions with torch and and return energies as numpy for plotting
 
-positions, energies = load_positions_and_energy(filename)
+positions, energies, n_samples, N, dim = load_positions_and_energy(filename)
 
-PINN_energies = evaluate_energy(model_name, positions)
+PINN_energies, PINN_log_wf = evaluate_energy(model_name, positions)
+
+
+PINN_log_wf_array = PINN_log_wf.reshape(-1)
 
 plt.plot(PINN_energies, label="PINN")
 plt.plot(energies, label="Analytical")
@@ -79,3 +84,39 @@ plt.xlabel("Sample")
 plt.ylabel("Energy")
 plt.legend()
 plt.show()
+
+
+if dim==1:
+    pos_array = positions.detach().cpu().numpy().reshape(-1)
+    wf_square = np.exp(2 * PINN_log_wf_array)
+
+    idx = np.argsort(pos_array)
+
+    x_sorted = pos_array[idx]
+    wf_sorted = wf_square[idx]
+
+    # Normalize PINN |psi|^2
+    norm = trapezoid(wf_sorted, x_sorted)
+    wf_sorted_norm = wf_sorted / norm
+
+    # Analytical |psi|^2
+    x = np.linspace(-3, 3, 500)
+    psi_sq_true = (1 / np.sqrt(np.pi)) * np.exp(-x**2)
+
+    # Plot unnormalized PINN
+    plt.plot(x_sorted, wf_sorted, label="PINN unnormalized", color="red")
+    plt.plot(x, psi_sq_true, label="Analytical", color="green")
+    plt.xlabel("x")
+    plt.ylabel(r"$|\psi(x)|^2$")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    # Plot normalized comparison
+    plt.scatter(x_sorted, wf_sorted_norm, s=5, label="PINN normalized",color="red")
+    plt.plot(x, psi_sq_true, label="Analytical", color="green")
+    plt.xlabel("x")
+    plt.ylabel(r"$|\psi(x)|^2$")
+    plt.legend()
+    plt.grid(True)
+    plt.show()