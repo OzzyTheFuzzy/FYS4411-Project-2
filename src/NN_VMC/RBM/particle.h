7df538fcda58b994c5136f927d81cd55f531729f
Teresa-Macarulla <teresm@uio.no>
2026-05-30 11:52:58 +0200

    README file

--- src/NN_VMC/RBM/particle.h
+++ src/NN_VMC/RBM/particle.h
@@ -5,6 +5,8 @@ class Particle {
 public:
     Particle(const std::vector<double>& position);
     void adjustPosition(double change, unsigned int dimension);
+    void setPosition(double value, unsigned int dimension);
+    void setPosition(const std::vector<double> loc);
     std::vector<double> &getPosition() { return m_position; }
     unsigned int getNumberOfDimensions() { return m_numberOfDimensions; }
 
